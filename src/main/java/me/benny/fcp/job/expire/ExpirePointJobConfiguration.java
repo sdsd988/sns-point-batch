d549779dfdecf9be84392a90a0ca7926964818c8
JUNGSANGYUN <sdsd98987@gmail.com>
2024-04-27 22:05:38 +0900

    feat: JobParameterValidator 추가
    날짜 파라미터를 검증하는 validator 추가

--- src/main/java/me/benny/fcp/job/expire/ExpirePointJobConfiguration.java
+++ src/main/java/me/benny/fcp/job/expire/ExpirePointJobConfiguration.java
@@ -1,10 +1,12 @@
 package me.benny.fcp.job.expire;
 
 
+import me.benny.fcp.job.validator.TodayJobParameterValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,10 +16,14 @@ public class ExpirePointJobConfiguration {
     @Bean
     public Job expirePointJob(
             JobBuilderFactory jobBuilderFactory,
+            TodayJobParameterValidator validator,
             Step expirePointStep
     ) {
         return jobBuilderFactory.get("expirePointJob")
                 .start(expirePointStep)
+                .validator(validator)
+                .incrementer(new RunIdIncrementer()) //run.id가 계속해서 증가해서 Job parameter 중복되지 않게 해줌
                 .build();
     }
+
 }
